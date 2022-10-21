445f01d8bc38ca06e4b8bed83fdad598be92b177
R Horo <notender_boy@ukr.net>
2022-10-21 21:42:19 +0300

    update

---
+++ js-loop/index.js
@@ -0,0 +1,19 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(arr)
+
+let i = 0;
+while (i < arr.length) {
+  console.log(arr[i])
+  i++
+}
+
+// for (let i = 0; i < arr.length; i += 2) {
+//   console.log(arr[i])
+// }
+
+// const arr2 = [4, 5, 6]
+// for (const x of arr2) {
+//   console.log(x)
+// }
+
+// arr.forEach(e => console.log(e))
